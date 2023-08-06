d3c40ce59b5fbecd4357ee3f4f6a36b80a8b0b27
Ankit <112419242+ankit999221@users.noreply.github.com>
2023-08-06 05:59:11 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import Navbar from "./components/Navbar";
-import {CarouselPart} from "./components/Carousel";
 import './App.css';
+import { Carousel } from "@material-tailwind/react";
 
 function App() {
+
+  const data = [
+    
+    { url: "https://normcph.com/new/2018/wp-content/uploads/2023/03/NormArchitects_Liewood_2023_Photoby_JonasBjerrePoulsen_Lowres_13.jpg", caption: "Photoby_JonasBjerrePoulsen_Lowres" },
+    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/10/NormArchitects_UDL_PhotoCredits_JonasBjerrePoulsen_06_cover-1.jpg", caption: "UDL_PhotoCredits_JonasBjerrePoulsen" },
+    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/09/NormArchitects_SoftMinimal_Gestalten_PhotoCredits_ChristianM%C3%B8llerAndersen_Lowres_29.jpg", caption: "SoftMinimal_Gestalten_PhotoCredits_Christian" },
+    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/07/NormArchitects_%C3%84ng_JonasBjerrePoulsen_Lowres_31-1.jpg", caption: "JonasBjerrePoulsen_Lowres" },
+    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/04/NormArchitects_Koio_JBP_lowres_14.jpg", caption: "Koio_JBP_lowres" },
+    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/02/2022.01.07_Norm_Architects_Xiamen_Basao_Tea_Store_0011_v1_LO-RES_COVER-1.jpg", caption: "Xiamen_Basao_Tea_Store" }
+  ]
+
   return (
-    <div className="App">
-    <Navbar/>
-    <CarouselPart/>
+    <div className="">
+      <Navbar />
+      <Carousel
+        loop={true}
+        autoplay={true}
+        autoplayDelay={4000}
+        transition={{duration:0.8}}
+        className=" overflow-hidden"
+        navigation={({ setActiveIndex, activeIndex, length }) => (
+          <div className="absolute bottom-16 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+            {new Array(length).fill("").map((_, i) => (
+              <span
+                key={i}
+                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+                  }`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
+        )}
+      >
+        {data.map((item, index) => (
+          <div key={index} style={{ backgroundImage: `url(${item.url})` }} className="max-h-screen min-h-screen customAnimate bg-center  bg-cover w-[120vw]">
+            
+            <span className="inset-0 h-screen w-screen justify-center flex items-center">
+              <div className="text-white font-semibold text-4xl">
+                {item.caption}
+              </div>
+
+            </span>
+          </div>
+        ))}
+
+      </Carousel>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,49 @@
 import React from "react";
-import { VscThreeBars } from 'react-icons/vsc';
-import { CiSearch } from 'react-icons/ci';
-import { RxCross1 } from 'react-icons/rx';
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+
+import { FaBars } from "react-icons/fa"
+import { AiOutlineSearch } from "react-icons/ai"
+
 
 function Navbar() {
+
+  const [open, setOpen] = React.useState(0);
+
+  const handleOpen = (value) => setOpen(open === value ? 0 : value);
   return (
     <>
-      <div className="title">
-      <div>
-        <a href="/">NORM ARCHITECTS</a>
-      </div>
-       <VscThreeBars/> 
-       <CiSearch/>
-       <RxCross1/>
-       
+      <div className="fixed overflow-hidden p-3 bg-[#f7f4ed] w-full z-20">
+        <Accordion className="p-4" open={open === 1} icon={<FaBars onClick={() => handleOpen(1)} className="" size={30} />}>
+          <AccordionHeader className="flex w-full">
+            <div className="uppercase font-medium justify-center w-full text-xl">
+              norm architects
+            </div>
+          </AccordionHeader>
+          <AccordionBody>
+            <div className=" mt-4 px-40 w-full flex items-center justify-center">
+              <div className="w-full border-t border-[#cec8bd]">
+                <div className="flex relative p-2">
+                  <div className="justify-center w-full text-center flex gap-3 items-center">
+                    <div className="cursor-pointer">Architecture</div>
+                    <div className="cursor-pointer">Design</div>
+                    <div className="cursor-pointer">Creative</div>
+                    <div className="cursor-pointer">Journal</div>
+                    <div className="cursor-pointer">Contact</div>
+                    <div className="cursor-pointer">Pressroom</div>
+                    <div className="cursor-pointer"></div>
+                  </div>
+                  <div className="absolute right-3">
+                    <AiOutlineSearch size={28} fill="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </AccordionBody>
+        </Accordion>
       </div>
     </>
   );
