5869a65fe7fd7c16605f9879e53c6660917b4b73
Ankit <112419242+ankit999221@users.noreply.github.com>
2023-08-06 06:07:28 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import Navbar from "./components/Navbar";
 import './App.css';
 import { Carousel } from "@material-tailwind/react";
+import data from "./Data.json"
 
 function App() {
-
-  const data = [
-    
-    { url: "https://normcph.com/new/2018/wp-content/uploads/2023/03/NormArchitects_Liewood_2023_Photoby_JonasBjerrePoulsen_Lowres_13.jpg", caption: "Photoby_JonasBjerrePoulsen_Lowres" },
-    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/10/NormArchitects_UDL_PhotoCredits_JonasBjerrePoulsen_06_cover-1.jpg", caption: "UDL_PhotoCredits_JonasBjerrePoulsen" },
-    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/09/NormArchitects_SoftMinimal_Gestalten_PhotoCredits_ChristianM%C3%B8llerAndersen_Lowres_29.jpg", caption: "SoftMinimal_Gestalten_PhotoCredits_Christian" },
-    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/07/NormArchitects_%C3%84ng_JonasBjerrePoulsen_Lowres_31-1.jpg", caption: "JonasBjerrePoulsen_Lowres" },
-    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/04/NormArchitects_Koio_JBP_lowres_14.jpg", caption: "Koio_JBP_lowres" },
-    { url: "https://normcph.com/new/2018/wp-content/uploads/2022/02/2022.01.07_Norm_Architects_Xiamen_Basao_Tea_Store_0011_v1_LO-RES_COVER-1.jpg", caption: "Xiamen_Basao_Tea_Store" }
-  ]
-
   return (
     <div className="">
       <Navbar />
